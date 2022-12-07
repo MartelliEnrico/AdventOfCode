3db49272f6392d5ffbe9fdd0fca235a0ab0ad9df
Enrico Martelli <mrcnrt95e30@gmail.com>
2022-12-07 10:06:29 +0100

    Day 7

--- src/main/kotlin/me/martelli/adventofcode/Day6.kt
+++ src/main/kotlin/me/martelli/adventofcode/Day6.kt
@@ -3,8 +3,11 @@ package me.martelli.adventofcode
 fun main() {
     val input = resource("day6.txt")
     val datastream = input.readText()
-    val startOfPacket = datastream.windowedSequence(4).indexOfFirst { it.toCharArray().toSet().size == 4 } + 4
-    val startOfMessage = datastream.windowedSequence(14).indexOfFirst { it.toCharArray().toSet().size == 14 } + 14
+    val startOfPacket = findUniqueSequence(datastream, 4)
+    val startOfMessage = findUniqueSequence(datastream, 14)
     println("1) Start of packet: $startOfPacket")
     println("2) Start of message: $startOfMessage")
 }
+
+private fun findUniqueSequence(input: String, size: Int) =
+    input.windowedSequence(size).indexOfFirst { it.toSet().size == size } + size
---
+++ src/main/kotlin/me/martelli/adventofcode/Day7.kt
@@ -0,0 +1,41 @@
+package me.martelli.adventofcode
+
+fun main() {
+    val input = resource("day7.txt")
+    val fileSystem: Entry.Dir = Entry.Dir()
+    lateinit var currentDir: Entry.Dir
+    input.forEachLine {
+        val components = it.split(" ")
+        when (components[0]) {
+            "$" -> when (components[1]) {
+                "cd" -> currentDir = when (components[2]) {
+                    "/" -> fileSystem
+                    ".." -> currentDir.parent!!
+                    else -> currentDir.content[components[2]] as Entry.Dir
+                }
+            }
+            "dir" -> currentDir.content[components[1]] = Entry.Dir(parent = currentDir)
+            else -> currentDir.content[components[1]] = Entry.File(components[0].toInt())
+        }
+    }
+    val dirSizes = fileSystem.allNestedDirs.map { it.size }
+    val sumOfDirsAtMost100000 = dirSizes.filter { it <= 100_000 }.sum()
+    val sizeToDelete = fileSystem.size - 40_000_000
+    val dirToDelete = dirSizes.filter { it >= sizeToDelete }.min()
+    println("1) Sum of dirs at most 100000: $sumOfDirsAtMost100000")
+    println("2) Size of dir to delete: $dirToDelete")
+}
+
+sealed class Entry {
+    abstract val size: Int
+
+    data class File(override val size: Int) : Entry()
+
+    data class Dir(val content: MutableMap<String, Entry> = mutableMapOf(), val parent: Dir? = null) : Entry() {
+        override val size: Int
+            get() = content.values.sumOf { it.size }
+
+        val allNestedDirs: List<Dir>
+            get() = content.values.mapNotNull { it as? Dir }.let { dirs -> dirs + dirs.flatMap { it.allNestedDirs } }
+    }
+}
