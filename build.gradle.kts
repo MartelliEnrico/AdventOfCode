4583b9bea9b7a36e279931b45e796540497d6e18
Enrico Martelli <mrcnrt95e30@gmail.com>
2024-12-11 15:21:32 +0100

    11/2024 - way faster implementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,6 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
     implementation("com.charleskorn.kaml:kaml:0.66.0")
-    implementation("io.arrow-kt:arrow-core:2.0.0")
-    implementation("io.arrow-kt:arrow-functions:2.0.0")
 }
 
 kotlin {
--- src/main/kotlin/me/martelli/adventofcode/year2024/Day11.kt
+++ src/main/kotlin/me/martelli/adventofcode/year2024/Day11.kt
@@ -1,7 +1,5 @@
 package me.martelli.adventofcode.year2024
 
-import arrow.core.MemoizedDeepRecursiveFunction
-import arrow.core.memoize
 import me.martelli.adventofcode.test
 
 fun main() = test(2024, 11, ::part1, ::part2)
@@ -10,31 +8,37 @@ private fun part1(input: String) = cycle(input, 25)
 
 private fun part2(input: String) = cycle(input, 75)
 
-private fun cycle(input: String, times: Int): Long = input.split(" ")
-    .map { it.toLong() }
-    .sumOf { blinkMultiple(it to times) }
+private fun cycle(input: String, steps: Int): Long {
+    var stones = input.split(" ")
+        .map { it.toLong() }
+        .associateWith { 1L }
 
-private val blinkMultiple = MemoizedDeepRecursiveFunction<Pair<Long, Int>, Long> { (stone, times) ->
-    val blinked = blinkMemo(stone)
-    if (times == 1) return@MemoizedDeepRecursiveFunction blinked.size.toLong()
-
-    var count = callRecursive(blinked[0] to times - 1)
-    if (blinked.size > 1) count += callRecursive(blinked[1] to times - 1)
-
-    return@MemoizedDeepRecursiveFunction count
-}
-
-private fun blink(stone: Long) = if (stone == 0L) {
-    arrayOf(1L)
-} else {
-    val s = stone.toString()
-    if (s.length % 2 == 0) {
-        val a = s.substring(0, s.length / 2).toLong()
-        val b = s.substring(s.length / 2).toLong()
-        arrayOf(a, b)
-    } else {
-        arrayOf(stone * 2024L)
+    for (i in 1..steps) {
+        stones = blink(stones)
     }
+
+    return stones.values.sum()
 }
 
-private val blinkMemo = ::blink.memoize()
+private fun blink(stones: Map<Long, Long>): Map<Long, Long> {
+    val newStones = mutableMapOf<Long, Long>().withDefault { 0L }
+
+    for ((stone, count) in stones.entries) {
+        if (stone == 0L) {
+            newStones.merge(1L, count, Long::plus)
+        } else {
+            val s = stone.toString()
+            if (s.length % 2 == 0) {
+                val a = s.substring(0, s.length / 2).toLong()
+                val b = s.substring(s.length / 2).toLong()
+
+                newStones.merge(a, count, Long::plus)
+                newStones.merge(b, count, Long::plus)
+            } else {
+                newStones.merge(stone * 2024L, count, Long::plus)
+            }
+        }
+    }
+
+    return newStones
+}
